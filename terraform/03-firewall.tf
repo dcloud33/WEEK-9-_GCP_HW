501753924abd80d94fc272b5bf36fe4c0dea7f96
Davey Wheeling <daveywheeling426@gmail.com>
2026-05-14 20:48:57 -0400

    edited terraform

--- terraform/03-firewall.tf
+++ terraform/03-firewall.tf
@@ -12,3 +12,21 @@ source_ranges = ["0.0.0.0/0"]
   
   target_tags = ["http-rule"]
 }
+
+resource "google_compute_firewall" "allow_health_checks" {
+  name    = "allow-google-health-checks"
+  network = google_compute_network.vpc_network.name
+
+  target_tags = ["http-rule"]
+# https://docs.cloud.google.com/load-balancing/docs/health-check-concepts?utm_source
+  source_ranges = [
+    "35.191.0.0/16",
+    "130.211.0.0/22"
+  ]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+}
--- terraform/06-health_checks.tf
+++ terraform/06-health_checks.tf
@@ -1,21 +1,3 @@
-resource "google_compute_firewall" "allow_health_checks" {
-  name    = "allow-google-health-checks"
-  network = google_compute_network.vpc_network.name
-
-  target_tags = ["http-rule"]
-# https://docs.cloud.google.com/load-balancing/docs/health-check-concepts?utm_source
-  source_ranges = [
-    "35.191.0.0/16",
-    "130.211.0.0/22"
-  ]
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-}
-
 resource "google_compute_health_check" "http_health_checks" {
   name         = "health-check"
  
